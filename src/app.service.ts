eb915c006de5738e0dca96abde9e20b8a466a51d
Deekshith Maram <s538365@nwmissouri.edu>
2021-01-20 22:23:30 -0600

    views add commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAbout(): string {
-    return 'This is the about page of Deekshith Maram-Software Developer.';
+    return 'Deekshith Maram-Software Developer.';
   }
   getHello(): string {
     return 'Hello World! This is Deekshith Maram.';
